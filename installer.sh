db7ef0d069599406fbb43458adcd2179d3014633
Alex Rao <alexrao17@gmail.com>
2016-07-19 15:20:49 -0700

    Fixed up the urls

--- installer.sh
+++ installer.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-
 TMP_FILE=/tmp/composer.tmp
+TMP_FILE_ONE=/tmp/composer_bytecode.tmp
 if [ -e /tmp/composer.tmp ]; then
   echo "Cleaning up from previous install failure"
   rm -f /tmp/composer.tmp
 fi
+if [ -e /tmp/composer_bytecode.tmp ]; then
+  echo "Cleaning up from previous install failure"
+  rm -f /tmp/composer_bytecode.tmp
+fi
 echo "Fetching latest version ..."
-curl --progress-bar http://www.peoplepowerco.com -o /tmp/composer.tmp
+curl --progress-bar https://raw.githubusercontent.com/peoplepower/composer-sdk-python/master/composer -o /tmp/composer.tmp
+curl --progress-bar https://raw.githubusercontent.com/peoplepower/composer-sdk-python/master/composer_bytecode -o /tmp/composer_bytecode.tmp
 if [ ! -d /usr/local/bin ]; then
   echo "Making /usr/local/bin"
   mkdir -p /usr/local/bin
 fi
 echo "Installing ..."
 sudo mv /tmp/composer.tmp /usr/local/bin/composer
+sudo mv /tmp/composer_bytecode.tmp /usr/local/bin/composer_bytecode
 chmod 755 /usr/local/bin/composer
