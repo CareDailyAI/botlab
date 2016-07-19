ef209eb1057738a512423c85a50abc1efad2d582
Alex Rao <alexrao17@gmail.com>
2016-07-19 14:47:07 -0700

    Added insaller file and composer bash script

---
+++ installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMP_FILE=/tmp/composer.tmp
+if [ -e /tmp/composer.tmp ]; then
+  echo "Cleaning up from previous install failure"
+  rm -f /tmp/composer.tmp
+fi
+echo "Fetching latest version ..."
+curl --progress-bar http://www.peoplepowerco.com/temp/composer/composer -o /tmp/composer.tmp
+if [ ! -d /usr/local/bin ]; then
+  echo "Making /usr/local/bin"
+  mkdir -p /usr/local/bin
+fi
+echo "Installing ..."
+sudo mv /tmp/composer.tmp /usr/local/bin/composer
+chmod 755 /usr/local/bin/composer
+
